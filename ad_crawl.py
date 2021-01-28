120a51c826f40392597660bb12f9949bb4b56343
袁浪 <yuanlang@bigsfintech.com>
2021-01-28 14:32:07 +0800

    mp4

--- ad_crawl.py
+++ ad_crawl.py
@@ -210,7 +210,7 @@ def video_download(limit, _file_name):
     #         t.submit(download, href)
     pools = []
     for href in hrefs:
-        _ip = ip_list.pop()
+        _ip = ip_list.pop() + ":31289"
         command = "you-get -o /root/project/youtube_crawl/video_dir -x {} --itag=18 {}".format(_ip, href)
         print("excute command >>>>> {0}".format(command))
         p = Process(target=download, args=(command,))
